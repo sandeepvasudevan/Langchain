ec209797fbecece9924ff8542846422d79bf4dc3
sandeepvasudevan <36402253+sandeepvasudevan@users.noreply.github.com>
2024-11-20 19:36:48 +0530

    Added comments to  QABot.py using Gemini Pro
    Gemini Pro QA Bot 
    My first GenAI App using LLM

--- QABot.py
+++ QABot.py
@@ -1,3 +1,4 @@
+#Library Imports
 import google.generativeai as genai
 import langchain_community
 import langchain_core
@@ -5,15 +6,20 @@ import langchain_groq
 import langchain_text_splitters
 import streamlit as str
 
+
+#Definitions
 API_KEY='yourownapikey'
 
+#Configure LLMS
 genai.configure(api_key=API_KEY)
 model=genai.GenerativeModel(model_name='gemini-pro')
 
+#Function to generate content from LLM
 def generate_content(question):
     response=model.generate_content(question)
     return response.text
 
+#Streamlit Section
 str.set_page_config(page_title='Demo QA')
 str.header('QA Chatbot')
 input=str.text_input('Input the question',key='input')
